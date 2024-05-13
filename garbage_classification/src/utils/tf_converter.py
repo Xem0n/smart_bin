37d1fb4592bfb744bd187dc29672a0d0087ef313
Szymon Biedukiewicz <biedukiewicz87@gmail.com>
2024-05-13 21:36:53 +0200

    optimize model

--- garbage_classification/src/utils/tf_converter.py
+++ garbage_classification/src/utils/tf_converter.py
@@ -12,6 +12,7 @@ def get_model(model_path):
 def convert_keras_to_lite(keras_path, lite_path):
     model = get_model(keras_path)
     converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
     tflite_model = converter.convert()
 
     with open(lite_path, 'wb') as file:
