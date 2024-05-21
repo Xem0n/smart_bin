37f39784c8a3951ac2048afc419d5d3d0c7882a3
Szymon Biedukiewicz <biedukiewicz87@gmail.com>
2024-05-21 14:36:02 +0200

    refactor resize() to transform_image()

--- webserver/src/services/utils.py
+++ webserver/src/services/utils.py
@@ -1,10 +1,20 @@
+import io
+import os
+import numpy as np
 from PIL import Image
 
-def resize(image_path, size):
-    print('resize')
-    with Image.open(image_path) as img:
-        img = img.resize(size, resample=Image.Resampling.LANCZOS)
-        img.save(image_path)
+def transform_image(id, bytes):
+    size = (os.getenv('IMG_WIDTH'), os.getenv('IMG_HEIGHT'))
+
+    image = Image.open(io.BytesIO(bytes))
+    image = image.resize(size, resample=Image.Resampling.LANCZOS)
+    image = image.convert(get_color_mode())
+    image.save(f'tmp/{id}_transformed.jpg')
+
+    return np.expand_dims(np.array(image), 0)
+
+def get_color_mode():
+    return 'RGB' if os.getenv('COLOR_MODE') == 'rgb' else 'L'
 
 def unify_bin_data(bins, categories, garbages):
     return [
