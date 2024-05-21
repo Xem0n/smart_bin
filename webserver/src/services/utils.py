13d6f66e2f52ef457def0e19596ff45060929531
Szymon Biedukiewicz <biedukiewicz87@gmail.com>
2024-05-21 21:10:34 +0200

    fix data structure

--- webserver/src/services/utils.py
+++ webserver/src/services/utils.py
@@ -17,26 +17,28 @@ def get_color_mode():
     return 'RGB' if os.getenv('COLOR_MODE') == 'rgb' else 'L'
 
 def unify_bin_data(bins, categories, garbages):
-    return [
-        {
-            'id': bin.id,
-            'name': bin.name,
-            'color': bin.color,
-            'categories': [
-                {
-                    'id': category.id,
-                    'name': category.name,
-                    'color': category.color,
-                    'garbages': [
-                        {
-                            'id': garbage.id,
-                            'created_at': garbage.created_at,
-                        }
-                        for garbage in garbages if garbage.category_id == category.id
-                    ],
-                }
-                for category in categories if category.bin_id == bin.id
-            ],
-        }
-        for bin in bins
-    ]
+    return {
+        'bins': [
+            {
+                'id': bin.id,
+                'name': bin.name,
+                'color': bin.color,
+                'categories': [
+                    {
+                        'id': category.id,
+                        'name': category.name,
+                        'color': category.color,
+                        'garbages': [
+                            {
+                                'id': garbage.id,
+                                'created_at': garbage.created_at,
+                            }
+                            for garbage in garbages if garbage.category_id == category.id
+                        ],
+                    }
+                    for category in categories if category.bin_id == bin.id
+                ],
+            }
+            for bin in bins
+        ]
+    }
