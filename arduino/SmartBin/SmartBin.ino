fb373d9ba18fef5ab67f34c41985518203886671
Szymon Biedukiewicz <biedukiewicz87@gmail.com>
2024-05-21 14:41:23 +0200

    feat: add stepper controller and caputure image in loop

--- arduino/SmartBin/SmartBin.ino
+++ arduino/SmartBin/SmartBin.ino
@@ -8,17 +8,29 @@
 #include "src/SDWrapper/SDWrapper.h"
 #include "src/ArduCAMWrapper/ArduCAMWrapper.h"
 #include "src/HTTPClient/HTTPClient.h"
+#include "src/StepperController/StepperController.h"
 
 using namespace SmartBin;
 
-int SD_PIN = A1;
-int ARDUCAM_PIN = 0;
+#define SD_PIN A1
+#define ARDUCAM_PIN A4
 
-char ssid[] = "ZielonePomarancze";
-char pass[] = "MurzynskieBananki";
+#define SEND_INTERVAL 10000
 
-HTTPClient httpClient("192.168.100.208", 5000);
+char ssid[] = "airbag";
+char pass[] = "glebogryzarka";
+
+HTTPClient httpClient("192.168.109.106", 5000);
 ArduCAM myCam;
+StepperController stepperController;
+
+size_t lastSendTime = millis();
+
+const char *garbageTypeMapping[] = {
+  "metal",
+  "paper",
+  "plastic"
+};
 
 void setup() {
   Serial.begin(9600);
@@ -35,7 +47,8 @@ void setup() {
 
   myCam = ArduCAMWrapper::init(ARDUCAM_PIN);
 
-  if (!SDWrapper::init(SD_PIN) || !WiFiWrapper::init(ssid, pass)) {
+  // if (!SDWrapper::init(SD_PIN) || !WiFiWrapper::init(ssid, pass)) {
+  if (!WiFiWrapper::init(ssid, pass)) {
     Serial.println("Initialization failed!");
     delay(60000);
   }
@@ -43,24 +56,22 @@ void setup() {
   Serial.println("Initialization done!");
   Serial.println();
 
-  sendImage();
+  // stepperController.drop(0);
+  // delay(1000);
+  // stepperController.drop(1);
+  // delay(1000);
+  // stepperController.drop(2);
+
+  // sendImage();
 }
 
 void sendImage() {
-  // File image = SD.open("metal.jpg", FILE_READ);
-  // File image = SD.open("paper.jpg", FILE_READ);
-  // File image = SD.open("plastic.jpg", FILE_READ);
-  // File image = SD.open("large_~1.png", FILE_READ);
-
   Serial.println("Sending image...");
 
   ArduCAMWrapper::Image image = ArduCAMWrapper::captureImage(&myCam);
-
   httpClient.sendRequest(image);
 
   delete[] image.data;
-
-  // image.close();
 }
 
 bool isGarbageType(HTTPResponse response) {
@@ -73,8 +84,23 @@ void loop() {
   if (response == DISCONNECTED) {
     Serial.println("Try once more...");
     sendImage();
+
+    lastSendTime = millis();
   } else if (isGarbageType(response)) {
-    Serial.println("Response:");
-    Serial.println(response);
+    Serial.print("Response: ");
+    Serial.println(garbageTypeMapping[response - 1]);
+
+    // stepperController.drop((int)response - 1);
+    lastSendTime = millis();
+  } else if (response == NO_REQUEST) {
+    size_t timestamp = millis();
+
+    if ((timestamp - lastSendTime) > SEND_INTERVAL) {
+      Serial.println("10s idle, sending image");
+      sendImage();
+      lastSendTime = timestamp;
+    }
+  } else if (response == UNKNOWN_TYPE) {
+    Serial.println("Unknown type");
   }
 }
