a6cb1d16418376c7164b49249f5cce4904681bcf
Szymon Biedukiewicz <biedukiewicz87@gmail.com>
2024-05-21 14:28:41 +0200

    remove unrelated stuff

--- webserver/src/services/predictor.py
+++ webserver/src/services/predictor.py
@@ -1,11 +1,5 @@
 import os
-import io
-import numpy as np
 from tensorflow import keras
-from PIL import Image
-from dotenv import load_dotenv
-
-load_dotenv()
 
 def load_model():
     model_path = os.getenv('MODEL_PATH')
@@ -19,8 +13,6 @@ def load_model():
 
 model = load_model()
 
-def convert_bytes_to_image_batch(bytes):
-    return np.expand_dims(np.array(Image.open(io.BytesIO(bytes))), 0)
 
 def predict(image):
     return model.predict(image)[0]
