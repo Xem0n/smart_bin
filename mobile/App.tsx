9e92ec9cfc2adccc1020451645ca7244257e2415
Szymon Biedukiewicz <biedukiewicz87@gmail.com>
2024-05-28 01:12:49 +0200

    add refresh

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,14 @@
 import "react-native-gesture-handler";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Resources, fetchResources } from "./src/api/resources";
 import { ActiveBinContext, ResourcesContext } from "./src/contexts";
 import Drawer from "./src/navigation/drawer/Drawer";
+import { RefreshControl, ScrollView } from "react-native";
 
 export default function App() {
   const [resources, setResources] = useState<Resources>();
   const [activeBin, setActiveBin] = useState<string | undefined>();
+  const [refreshing, setRefreshing] = useState<boolean>(false);
 
   const fetchData = async () => {
     try {
@@ -25,12 +27,28 @@ export default function App() {
       .catch(console.error);
   }, []);
 
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+
+    fetchData()
+      .then((resources) => setActiveBin(resources?.bins[0].id))
+      .catch(console.error)
+      .finally(() => setRefreshing(false));
+  }, []);
+
   return (
     <ResourcesContext.Provider value={{ data: resources, update: fetchData }}>
       <ActiveBinContext.Provider
         value={{ data: activeBin, update: setActiveBin }}
       >
-        <Drawer />
+        <ScrollView
+          contentContainerStyle={{ flexGrow: 1 }}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+        >
+          <Drawer />
+        </ScrollView>
       </ActiveBinContext.Provider>
     </ResourcesContext.Provider>
   );
